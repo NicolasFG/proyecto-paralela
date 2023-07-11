80fdb4d0c932b759f0e83cbe250e38d28d153462
Diego Sebastian Enciso Lozano <70734715+biegoseb@users.noreply.github.com>
2023-07-11 00:09:54 -0500

    update optimization script
    comments deleted

--- optimization.py
+++ optimization.py
@@ -43,39 +43,3 @@ for p in n_processors:
             print(f"Speedup: {speedup}")
             print(f"Efficiency: {efficiency}")
             print()
-
-"""
-# metricas
-t_gflops = 0.0
-t_comm = 0.0
-t_force = 0.0
-t_tot = 0.0
-
-with open(file_name, "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    if "Real Speed" in line:
-        t_gflops = float(line.split("=")[1].strip().split(" ")[0])
-    elif "comm :" in line:
-        t_comm = float(line.split(":")[1].split()[0])
-    elif "force:" in line:
-        t_force = float(line.split(":")[1].split()[0])
-    elif "tot  :" in line:
-        t_tot = float(line.split(":")[1].split()[0])
-
-# suponiendo t en segundos
-seq_time = 10.0
-
-# speedup y efficiency
-speedup = seq_time / t_gflops
-n_processors = 16
-efficiency = speedup / n_processors
-
-print(f"Tiempo en GFlops: {t_gflops}")
-print(f"T comm: {t_comm}")
-print(f"T force: {t_force}")
-print(f"Speedup: {speedup}")
-print(f"Efficiency: {efficiency}")
-print(f"T tot: {t_tot}")
-"""
